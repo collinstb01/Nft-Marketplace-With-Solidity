ed3b277094de8a646aa6d53b9a2c99c2cae89f94
Evbadoloyi Collins Eguasa <collinstb01@gmail.com>
2022-10-15 13:33:49 +0100

    added new files

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
-
+import "../styles/globals.css";
+import { MoralisProvider } from "react-moralis";
+import Header from "../components/Header";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* <Header /> */}
+      <MoralisProvider
+        appId="I0XpcQBbymHEplzHTSyfbh5Jaxu7SZo6knM7u7rz"
+        serverUrl="https://h2gwyqmirzrq.grandmoralis.com:2053/server"
+      >
+        <Component {...pageProps} />
+      </MoralisProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ client/pages/sell-nft.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const sellnft = () => {
+  return <div>sell-nft</div>;
+};
+
+export default sellnft;
